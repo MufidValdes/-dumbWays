7612d6e937324ed57beaf0c62d08f9c7a8a6df70
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-02 09:25:47 +0700

    Week3 Day4

---
+++ W3D4-Data-Modeling/app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// set global variable
+app.set("view engine", "hbs")
+app.set("views", "src/views");
+
+// akses file 
+app.use("/assets",express.static(path.join(__dirname, 'src/assets')))
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get('/', home)
+app.get('/contact', contact)
+app.get('/myproject', myproject)
+app.get('/detail', detail)
+app.get('/testimonial', testimonial)
+
+
+function home(req, res) {
+    res.render('index')
+}
+
+function contact(req, res) {
+    res.render('contact')
+}
+
+function myproject(req, res) {
+    const data = [
+        {
+            title: "Title 1",
+            content: "Content 1"
+        },
+        {
+            title: "Title 2",
+            content: "Content 2"
+        },
+        {
+            title: "Title 3",
+            content: "Content 3"
+        }
+    ]
+
+    res.render('myproject', { data })
+}
+
+function detail(req, res) {
+    res.render('detail')
+}
+
+function testimonial(req, res) {
+    res.render('testimonial')
+}
+
+
+app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`)
+})
+
+// ==============================
+// // Basic routing
+// ==============================
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+//   app.post('/', (req, res) => {
+//     res.send('Got a POST request')
+//   })
+//   app.put('/user', (req, res) => {
+//     res.send('Got a PUT request at /user')
+//   })
+//   app.delete('/user', (req, res) => {
+//     res.send('Got a DELETE request at /user')
+//   })
---
+++ W3D4-Data-Modeling/src/assets/js/navbar.js
@@ -0,0 +1,5 @@
+function toggleNavbar() {
+    const element = document.getElementById("hamburger-container");
+    element.classList.toggle("hamburger-hide");
+  }
+  
